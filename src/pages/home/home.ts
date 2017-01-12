fc8e3a580e8d8546615542d5301298966ae32032
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2017-01-12 11:20:55 +0100

    Configured Google Analytics without plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { FirebaseAuth } from 'angularfire2';
 import { Component } from '@angular/core';
 
+declare var dataLayer: Array<any>;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,7 +11,22 @@ export class HomePage {
 
   constructor(private fireAuth: FirebaseAuth) {}
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    dataLayer.push({
+      'screenPath': 'home',
+      'screenName': 'Home'
+    });
+    dataLayer.push({'event': 'appScreenView'});
+  }
+
   logout() {
+    dataLayer.push({
+      'appEventCategory': 'home',
+      'appEventAction': 'click',
+      'appEventLabel': 'logout'
+    });
+    dataLayer.push({'event': 'appEvent'});
     this.fireAuth.logout();
   }
 
